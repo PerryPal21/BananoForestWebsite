95ae87c3d0bb8e32465b8901c9f6852867eaa89c
Glitch (glitch-hello-node) <none>
2022-03-01 14:27:50 +0000

    🎨💫 Checkpoint ./src/pages/index.hbs:40555805/21971 ./server.js:40555805/1787 ./src/pages/raffle.hbs:40555805/31624 ./src/pages/faq.hbs:40555805/6347

--- server.js
+++ server.js
@@ -22,56 +22,24 @@ fastify.register(require("point-of-view"), {
 
 fastify.get("/", function(request, reply) {
   var params = {}
-  reply.view("/src/views/index.hbs", params);
-});
-
-/**
-* Our POST route to handle and react to form submissions 
-*
-* Accepts body data indicating the user choice
-*/
-fastify.post("/", function(request, reply) {
-  
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-  
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-  
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-    
-    // Load our color data file
-    const colors = require("./src/colors.json");
-    
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-    
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo
-      };
-    } else {
-      
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo
-      };
-    }
-  }
-  
-  // The Handlebars template will use the parameter values to update the page with the chosen color
   reply.view("/src/pages/index.hbs", params);
 });
 
-// Run the server and report out to the logs
+fastify.get("/raffle", function(request, reply) {
+  var params = {}
+  reply.view("/src/pages/raffle.hbs", params);
+});
+
+fastify.get("/faq", function(request, reply) {
+  var params = {}
+  reply.view("/src/pages/faq.hbs", params);
+});
+
+fastify.get("/api", function(request, reply) {
+  
+});
+
+
 fastify.listen(process.env.PORT, '0.0.0.0', function(err, address) {
   if (err) {
     fastify.log.error(err);

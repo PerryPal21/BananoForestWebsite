1e6b21d39cefe47a9c24e78d4a59b89026315d45
Glitch (glitch-hello-node) <none>
2022-03-01 14:15:49 +0000

    🌍🏋 Checkpoint ./server.js:40555805/690

--- server.js
+++ server.js
@@ -1,46 +1,27 @@
 const path = require("path");
 
-// Require the fastify framework and instantiate it
 const fastify = require("fastify")({
-  // Set this to true for detailed logging:
   logger: false
 });
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
 
 
-// Setup our static files
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
   prefix: "/" // optional: default '/'
 });
 
-// fastify-formbody lets us parse incoming forms
 fastify.register(require("fastify-formbody"));
 
-// point-of-view is a templating manager for fastify
 fastify.register(require("point-of-view"), {
   engine: {
     handlebars: require("handlebars")
   }
 });
 
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
 
-/**
-* Our home page route
-*
-* Returns src/pages/index.hbs with data built into it
-*/
 fastify.get("/", function(request, reply) {
-  
-  // params is an object we'll pass to our handlebars template
- params = {}
-  // The Handlebars code will be able to access the parameter values and build them into the page
+  var params = {}
   reply.view("/src/views/index.hbs", params);
 });
 
